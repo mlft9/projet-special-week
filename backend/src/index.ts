c785a66805d9cbc5ad1512b2309a813c5045c40b
unknown <maxime.lefort@etik.com>
2026-03-19 13:14:04 +0100

    fix backend proxy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ import adminRouter from './routes/admin'
 const app = express()
 const PORT = process.env.PORT ?? 3001
 
+app.set('trust proxy', 1)
 app.use(express.json({ limit: '10kb' }))
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
